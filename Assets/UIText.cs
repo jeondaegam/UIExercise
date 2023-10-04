366d210867dde6952e375dcf86e34cde7011d9ed
Yeoreum <jeondaegam@naver.com>
2023-10-04 13:52:22 +0900

    Add SlidingNumber
    - Target number를 입력하면
    - Current number가 TargetNumber까지 자연스럽게 증가

--- Assets/UIText.cs
+++ Assets/UIText.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using TMPro;
 using UnityEngine;
 
 public class UIText : MonoBehaviour
 {
     public TextMeshProUGUI Text;
+    public int TargetNumber;
+    public float CurrentNumber;
+    public float Speed = 10;
+    public float Duration; // 2초만에 업데이트 되도록 
 
     private void Awake()
     {
@@ -21,6 +26,31 @@ public class UIText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SlidingNumber();
         
+    }
+
+    private void SlidingNumber()
+    {
+        if (CurrentNumber != TargetNumber)
+        {
+            if (CurrentNumber < TargetNumber)
+            {
+                CurrentNumber += Time.deltaTime * Speed;
+                if (CurrentNumber > TargetNumber)
+                {
+                    CurrentNumber = TargetNumber;
+                }
+            }
+            else
+            {   // TargetNumber가 더 큰경우
+                CurrentNumber -= Time.deltaTime * Speed;
+                if (CurrentNumber < TargetNumber)
+                {
+                    CurrentNumber = TargetNumber;
+                }
+            }
+        }
+        Text.text = ((int)CurrentNumber).ToString();
     }
 }
