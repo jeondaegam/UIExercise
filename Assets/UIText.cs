8b372f8b5115190f2e9d0828b6031dc77d4d956c
Yeoreum <jeondaegam@naver.com>
2023-10-04 16:20:57 +0900

    Add 값 자동변화 (Lerp or Speed)
    - Speed Based
    - Lerp Based

--- Assets/UIText.cs
+++ Assets/UIText.cs
@@ -6,19 +6,27 @@ using UnityEngine;
 
 public class UIText : MonoBehaviour
 {
+    public enum ESlidingMethod { None, SpeedBased, LerpBased } // enum type 선언 
+    public ESlidingMethod SlidingMethod; // 
+
     public TextMeshProUGUI Text;
     public int TargetNumber;
-    public float CurrentNumber;
-    public float Speed = 10;
-    public float Duration = 5; // 2초만에 업데이트 되도록
-
     public float LastTargetNumber;
+    public float Duration = 5; // n초만에 업데이트 되도록 설청한
+    public float LastDuration;
 
+
+    [Header("Speed Based")]
+    public float Speed = 10;
+    public float CurrentNumber;
+
+
+    [Header("Lerp Based")]
+    public bool DoLerp;
     public float StartNumber;
     public float EndNumber;
     public float ElapsedTime;
 
-    public bool DoLerp;
 
 
     private void Awake()
@@ -40,13 +48,32 @@ public class UIText : MonoBehaviour
     void Update()
     {
         Lerp();
-        //UpdateSpeed();
+        //UpdateSpeed(); // 제한시간내에 목표값에 도달하기 위해 속도에 계속해서 변화를 준다 
         //SlidingNumber();
+
+        switch (SlidingMethod)
+        {
+            case ESlidingMethod.SpeedBased:
+                UpdateSpeed();
+                SlidingNumber();
+                break;
+
+            case ESlidingMethod.LerpBased:
+                if ((TargetNumber != LastDuration) || (Duration != LastDuration))
+                {
+                    StartLerp();
+                    LastTargetNumber = TargetNumber;
+                    LastDuration = Duration;
+                }
+                Lerp();
+                break;
+        }
+
     }
 
     private void UpdateSpeed()
     {
-        if(LastTargetNumber != TargetNumber)
+        if (LastTargetNumber != TargetNumber)
         {
             // 속도 = (변화량:목표값과 현재값 사이의 차이) / 시간
             // 남은 변화량이 시간내에 소진되도록 속도를 수정한다
@@ -60,7 +87,7 @@ public class UIText : MonoBehaviour
         // 유저가 코인을 획득한다면 ?! 이 함수를 호출
         DoLerp = true;
         int.TryParse(Text.text, out int textValue);
-        StartNumber = (float)textValue;
+        StartNumber = textValue;
         ElapsedTime = 0;
     }
 
@@ -86,7 +113,7 @@ public class UIText : MonoBehaviour
                 FinishLerp();
                 DoLerp = true;
                 int.TryParse(Text.text, out int textValue);
-                StartNumber = (float)textValue;
+                StartNumber = textValue;
                 ElapsedTime = 0;
             }
 
@@ -96,23 +123,21 @@ public class UIText : MonoBehaviour
 
     private void SlidingNumber()
     {
-        if (CurrentNumber != TargetNumber)
+        if (CurrentNumber < TargetNumber)
         {
+
+            CurrentNumber += Time.deltaTime * Speed;
+            if (CurrentNumber > TargetNumber)
+            {
+                CurrentNumber = TargetNumber;
+            }
+        }
+        else if (CurrentNumber > TargetNumber)
+        {   // TargetNumber가 더 큰경우
+            CurrentNumber -= Time.deltaTime * Speed;
             if (CurrentNumber < TargetNumber)
             {
-                CurrentNumber += Time.deltaTime * Speed;
-                if (CurrentNumber > TargetNumber)
-                {
-                    CurrentNumber = TargetNumber;
-                }
-            }
-            else
-            {   // TargetNumber가 더 큰경우
-                CurrentNumber -= Time.deltaTime * Speed;
-                if (CurrentNumber < TargetNumber)
-                {
-                    CurrentNumber = TargetNumber;
-                }
+                CurrentNumber = TargetNumber;
             }
         }
         Text.text = ((int)CurrentNumber).ToString();
