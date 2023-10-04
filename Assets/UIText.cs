a77d5f5fd609c4fb4c8bca88a45c1fa3959f46e2
Yeoreum <jeondaegam@naver.com>
2023-10-04 15:08:55 +0900

    Add Lerp함수

--- Assets/UIText.cs
+++ Assets/UIText.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using TMPro;
 using UnityEngine;
 
@@ -10,11 +10,24 @@ public class UIText : MonoBehaviour
     public int TargetNumber;
     public float CurrentNumber;
     public float Speed = 10;
-    public float Duration; // 2초만에 업데이트 되도록 
+    public float Duration = 5; // 2초만에 업데이트 되도록
+
+    public float LastTargetNumber;
+
+    public float StartNumber;
+    public float EndNumber;
+    public float ElapsedTime;
+
+    public bool DoLerp;
+
 
     private void Awake()
     {
         Text = GetComponent<TextMeshProUGUI>();
+        TargetNumber = 10000;
+        CurrentNumber = 0;
+        LastTargetNumber = -1;
+        Text.text = "0";
     }
 
     // Start is called before the first frame update
@@ -26,9 +39,60 @@ public class UIText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SlidingNumber();
-        
+        Lerp();
+        //UpdateSpeed();
+        //SlidingNumber();
     }
+
+    private void UpdateSpeed()
+    {
+        if(LastTargetNumber != TargetNumber)
+        {
+            // 속도 = (변화량:목표값과 현재값 사이의 차이) / 시간
+            // 남은 변화량이 시간내에 소진되도록 속도를 수정한다
+            //Speed = (TargetNumber - CurrentNumber) / Duration;
+        }
+    }
+
+    //Lerp를 시작하는 함수
+    public void StartLerp()
+    {
+        // 유저가 코인을 획득한다면 ?! 이 함수를 호출
+        DoLerp = true;
+        int.TryParse(Text.text, out int textValue);
+        StartNumber = (float)textValue;
+        ElapsedTime = 0;
+    }
+
+    public void FinishLerp()
+    {
+        DoLerp = false;
+        Text.text = TargetNumber.ToString();
+    }
+
+
+
+    private void Lerp()
+    {
+        if (DoLerp)
+        {
+            // 경과시간
+            ElapsedTime += Time.deltaTime;
+            float value = Mathf.Lerp(StartNumber, EndNumber, ElapsedTime / Duration);
+            Text.text = ((int)value).ToString();
+
+            if (ElapsedTime > Duration)
+            {
+                FinishLerp();
+                DoLerp = true;
+                int.TryParse(Text.text, out int textValue);
+                StartNumber = (float)textValue;
+                ElapsedTime = 0;
+            }
+
+        }
+    }
+
 
     private void SlidingNumber()
     {
